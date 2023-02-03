5a0fe7322cfcc529441e4f3350ba5b8e3741d6c6
Nekipel <nekipelovaa@icloud.com>
2023-02-03 21:19:44 +0300

    add function translateToRomanFromArabic from task

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
+    public static boolean isRomanNumber = false;
     public static String[] arrRomanNumber = {"I","II","III","IV","V","VI","VII","VII","IX","X"};
+    public static String[] arrRomanNumberTen = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC","C"};
     public static String[] arrArabicNumber = {"0","1","2","3","4","5","6","7","8","9","10"};
     public static String expression; // Создал переменную для уравнения
     public static void main(String[] args) {
@@ -20,6 +22,7 @@ public class Main {
         String value2 = example[2];
         String value3 = example[1];
         if ((Arrays.asList(arrRomanNumber).contains(value1)) & (Arrays.asList(arrRomanNumber).contains(value2))){
+            isRomanNumber = true;
             String number1 = String.valueOf(Arrays.binarySearch(arrRomanNumber, value1));
             String number2 = String.valueOf(Arrays.binarySearch(arrRomanNumber, value2));
             int valueNumber1 = Integer.parseInt(number1) + 1;
@@ -55,26 +58,68 @@ public class Main {
         switch (example[1]) {
             case "+" -> {
                 result = firstValue + secondValue;
-                System.out.println(result);
+                if(isRomanNumber) {
+                    translateToRomanFromArabic(result);
+                } else {
+                    System.out.println(result);
+                }
                 return result.toString();
             }
             case "-" -> {
                 result = firstValue - secondValue;
-                System.out.println(result);
+                if(isRomanNumber) {
+                    translateToRomanFromArabic(result);
+                } else {
+                    System.out.println(result);
+                }
                 return result.toString();
             }
             case "/" -> {
                 result = firstValue / secondValue;
-                System.out.println(result);
+                if(isRomanNumber) {
+                    translateToRomanFromArabic(result);
+                } else {
+                    System.out.println(result);
+                }
                 return result.toString();
             }
             case "*" -> {
                 result = firstValue * secondValue;
-                System.out.println(result);
+                if(isRomanNumber) {
+                    translateToRomanFromArabic(result);
+                } else {
+                    System.out.println(result);
+                }
                 return result.toString();
+            }
+            default -> {
+                System.out.println("throws Exception");
             }
         }
 
         return "";
     } // Функция калькулирования арабский цифр
+    public static String translateToRomanFromArabic (Integer input) {
+        String Answer = input.toString();
+
+        if(Answer.length() == 3){
+            System.out.println(arrRomanNumberTen[10]);
+            return arrRomanNumberTen[10];
+        }
+        if(Answer.length() == 1) {
+            System.out.println(arrRomanNumber[Integer.parseInt(Answer) - 1]);
+            return arrRomanNumber[Integer.parseInt(Answer) - 1];
+        }
+        String[] example = new String[Answer.length()];
+        for (int i = 0; i < Answer.length(); i++ ) {
+            example[i] = String.valueOf(Answer.charAt(i));
+        }
+        if(input % 10 == 0){
+            Answer = arrRomanNumberTen[Integer.parseInt(example[0])];
+        } else {
+            Answer = arrRomanNumberTen[Integer.parseInt(example[0])] + arrRomanNumber[Integer.parseInt(example[1]) - 1];
+        }
+        System.out.println(Answer);
+        return Answer;
+    } // функция возврата римского числа
 }
